2ce817a0cfc3286eeef9d4a16df7331e4832fd96
jean <jean@jean.com>
2015-06-22 20:40:04 +0800

    initial player

---
+++ othello/supermanplayer.py
@@ -0,0 +1,15 @@
+class SupermanPlayer:
+	def __init__(self,color):
+		self.color = color
+		
+	def chooseMove(self, board, prevMove):
+		return
+
+	def gameEnd(self, board):
+		return
+
+	def getColor(self):
+		return self.color
+
+	def getMemoryUsedMB(self):
+		return 0.0
